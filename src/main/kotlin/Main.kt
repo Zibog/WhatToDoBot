31bc389e25df4f8318cd68f0b1d55dae6b4fd2ed
Zibog <dmsidak@gmail.com>
2025-02-02 23:17:17 +0200

    Remove unused imports

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,6 @@ package com.dsidak
 
 import com.dsidak.bot.loadProperties
 import com.dsidak.bot.runEchoBot
-import java.io.File
-import java.util.*
 
 fun main() {
     val properties = loadProperties()
