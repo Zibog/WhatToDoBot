9359323176d375a2738a2e1c3ce94399bad71467
Dmitry Sidak <dmsidak@gmail.com>
2025-02-22 16:55:13 +0200

    Disable tests until TelegramBots
    Signed-off-by: Dmitry Sidak <dmsidak@gmail.com>

--- src/test/kotlin/bot/WeatherBotTest.kt
+++ src/test/kotlin/bot/WeatherBotTest.kt
@@ -3,11 +3,13 @@ package bot
 import com.dsidak.bot.TgBot
 import com.dsidak.bot.WeatherBot
 import com.dsidak.bot.loadProperties
+import org.junit.jupiter.api.Disabled
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
+@Disabled("Wait for TelegramBots")
 class WeatherBotTest {
     private lateinit var bot: TgBot
 
