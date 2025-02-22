25f020e37c2a7c293d289e453137b956bfbc6ee6
Dmitry Sidak <dmsidak@gmail.com>
2025-02-22 20:18:13 +0200

    Introduce logging
    Signed-off-by: Dmitry Sidak <dmsidak@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ group = "com.dsidak"
 version = "1.0-SNAPSHOT"
 
 val telegramBots: String by project
+val kotlinLogging: String by project
+val logback: String by project
 val mockito: String by project
 
 repositories {
@@ -14,10 +16,18 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.telegram/telegrambots-client
     implementation("org.telegram:telegrambots-client:$telegramBots")
+    // https://mvnrepository.com/artifact/org.telegram/telegrambots-abilities
     implementation("org.telegram:telegrambots-abilities:$telegramBots")
 
+    // https://mvnrepository.com/artifact/io.github.oshai/kotlin-logging-jvm
+    implementation("io.github.oshai:kotlin-logging-jvm:$kotlinLogging")
+    // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+    implementation("ch.qos.logback:logback-classic:")
+
     testImplementation(kotlin("test"))
+    // https://mvnrepository.com/artifact/org.mockito/mockito-core
     testImplementation("org.mockito:mockito-core:$mockito")
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,24 +2,29 @@ package com.dsidak
 
 import com.dsidak.bot.BotProperties
 import com.dsidak.bot.WeatherBot
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication
 
+val log = KotlinLogging.logger {}
+
 fun main() {
     val token = BotProperties.BOT_API_KEY
+    val botUsername = BotProperties.BOT_USERNAME
+    log.debug { "$botUsername starts using token=$token" }
     try {
         // Instantiate Telegram Bots API
         TelegramBotsLongPollingApplication().use { botsApplication ->
             // Create Bot
-            val bot = WeatherBot(OkHttpTelegramClient(token), BotProperties.BOT_USERNAME)
+            val bot = WeatherBot(OkHttpTelegramClient(token), botUsername)
             // Enable abilities
             bot.onRegister()
             // Register our bot
             botsApplication.registerBot(token, bot)
-            println("${bot.javaClass.simpleName} successfully started")
+            log.info { "${bot::class.java} successfully started" }
             Thread.currentThread().join()
         }
     } catch (e: Exception) {
-        e.printStackTrace()
+        log.error { e }
     }
 }
