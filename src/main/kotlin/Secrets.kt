e3a6e1b8eecb080bd493a4391163817a3dc85a07
Dmitry Sidak <dmsidak@gmail.com>
2025-02-23 22:32:05 +0200

    Move API keys, etc to separate file
    Signed-off-by: Dmitry Sidak <dmsidak@gmail.com>

--- src/main/kotlin/Secrets.kt
+++ src/main/kotlin/Secrets.kt
@@ -3,10 +3,8 @@ package com.dsidak
 object Secrets {
     // Telegram Bot API key
     const val BOT_API_KEY = ""
-
     // Telegram Bot username
     const val BOT_USERNAME = ""
-
     // Telegram Bot creator ID
     const val BOT_CREATOR_ID = 0L
 
