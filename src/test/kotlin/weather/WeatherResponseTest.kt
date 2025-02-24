576fd005e9aba52fc5174f9db6c4e76435426f6d
Dmitry Sidak <dmsidak@gmail.com>
2025-02-24 02:29:30 +0200

    Add another test JSON
    Signed-off-by: Dmitry Sidak <dmsidak@gmail.com>

--- src/test/kotlin/weather/WeatherResponseTest.kt
+++ src/test/kotlin/weather/WeatherResponseTest.kt
@@ -11,10 +11,10 @@ class WeatherResponseTest {
 
     @Test
     fun testDecodeJson_current_city() {
-        val resourceName = "Newtonhill_GB.json"
-        val file = File("$path/$resourceName")
+        var resourceName = "Newtonhill_GB_current.json"
+        var file = File("$path/$resourceName")
 
-        val weatherResponse = Json.decodeFromString<WeatherResponse>(file.readText())
+        var weatherResponse = Json.decodeFromString<WeatherResponse>(file.readText())
         checkCoordinates(-2.15, 57.0, weatherResponse.coordinates)
         assertEquals(1, weatherResponse.weather.size)
         checkWeather(804, "Clouds", "overcast clouds", "04d", weatherResponse.weather[0])
@@ -29,6 +29,25 @@ class WeatherResponseTest {
         assertEquals(2641549, weatherResponse.id)
         assertEquals("Newtonhill", weatherResponse.name)
         assertEquals(200, weatherResponse.code)
+
+        resourceName = "Sofia_BG_current.json"
+        file = File("$path/$resourceName")
+
+        weatherResponse = Json.decodeFromString<WeatherResponse>(file.readText())
+        checkCoordinates(23.3242, 42.6975, weatherResponse.coordinates)
+        assertEquals(1, weatherResponse.weather.size)
+        checkWeather(804, "Clouds", "overcast clouds", "04n", weatherResponse.weather[0])
+        assertEquals("stations", weatherResponse.base)
+        checkMain(-1.57, -6.96, -2.17, -1.57, 1033, 68, weatherResponse.main)
+        assertEquals(10000, weatherResponse.visibility)
+        checkWind(5.14, 100, wind = weatherResponse.wind)
+        checkClouds(100, weatherResponse.clouds)
+        assertEquals(1740356208, weatherResponse.dt)
+        checkSys(2, 2033225, "BG", 1740373841, 1740413370, weatherResponse.sys)
+        assertEquals(7200, weatherResponse.timezone)
+        assertEquals(727011, weatherResponse.id)
+        assertEquals("Sofia", weatherResponse.name)
+        assertEquals(200, weatherResponse.code)
     }
 
     private fun checkCoordinates(longitude: Double, latitude: Double, coordinates: Coordinates) {
@@ -36,6 +55,7 @@ class WeatherResponseTest {
         assertEquals(latitude, coordinates.latitude)
     }
 
+    @Suppress("SameParameterValue")
     private fun checkWeather(id: Int, main: String, description: String, icon: String, weather: Weather) {
         assertEquals(id, weather.id)
         assertEquals(main, weather.main)
@@ -66,10 +86,12 @@ class WeatherResponseTest {
         assertEquals(gust, wind.gust)
     }
 
+    @Suppress("SameParameterValue")
     private fun checkClouds(all: Int, clouds: Clouds) {
         assertEquals(all, clouds.all)
     }
 
+    @Suppress("SameParameterValue")
     private fun checkSys(type: Int, id: Int, country: String, sunrise: Long, sunset: Long, sys: Sys) {
         assertEquals(type, sys.type)
         assertEquals(id, sys.id)
