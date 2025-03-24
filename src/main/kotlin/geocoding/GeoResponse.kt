052f2ff3bbf2d3d0b04b9e09003d4172700eb6c7
Dmitry Sidak <dmsidak@gmail.com>
2025-03-24 02:26:16 +0200

    Add GeoResponse
    Signed-off-by: Dmitry Sidak <dmsidak@gmail.com>

---
+++ src/main/kotlin/geocoding/GeoResponse.kt
@@ -0,0 +1,17 @@
+package com.dsidak.geocoding
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CityInfo(
+    val name: String,
+    @SerialName("local_names")
+    val localNames: Map<String, String> = emptyMap(),
+    @SerialName("lat")
+    val latitude: Double,
+    @SerialName("lon")
+    val longitude: Double,
+    val country: String,
+    val state: String
+)
