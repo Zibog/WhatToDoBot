7d342b4d3e13fae9185ac9d8e605f973de392754
Dmitry Sidak <dmsidak@gmail.com>
2025-03-30 01:29:37 +0200

    Add startup time in log
    Signed-off-by: Dmitry Sidak <dmsidak@gmail.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ import io.github.cdimascio.dotenv.dotenv
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication
+import java.time.Instant
 
 val log = KotlinLogging.logger {}
 
@@ -12,6 +13,7 @@ fun main() {
     val token = dotenv["BOT_API_KEY"]
     val botUsername = dotenv["BOT_USERNAME"]
     log.debug { "$botUsername starts using token=$token" }
+    val start = Instant.now()
     try {
         // Instantiate Telegram Bots API
         TelegramBotsLongPollingApplication().use { botsApplication ->
@@ -21,7 +23,7 @@ fun main() {
             bot.onRegister()
             // Register our bot
             botsApplication.registerBot(token, bot)
-            log.info { "${bot::class.java} successfully started" }
+            log.info { "${bot::class.java} successfully started in ${Instant.now().toEpochMilli() - start.toEpochMilli()}ms" }
             Thread.currentThread().join()
         }
     } catch (e: Exception) {
